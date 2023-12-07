eaa6863017950827e992788344e9b76cef0c5af6
alienshy <alisahverdiyeW8@gmail.com>
2023-12-07 17:31:54 +0400

    sabah

--- JS/dts.js
+++ JS/dts.js
@@ -1,18 +1,17 @@
 let id = new URLSearchParams(window.location.search).get("id")
-const robots = document.querySelector('.robot-two')
+const robots = document.querySelector('.robotdivclass')
 
 fetch(`http://localhost:3000/robots/${id}`)
     .then(response => response.json())
     .then(robot => {
+        console.log(robot);
         robots.innerHTML += `
-        <div class="sectioncon">
-                        <div class="blok-one">
-                            <img src="${robot.image}" alt="">
-                        </div>
-                        <div class="blok-two">
-                            <h3>${robot.name}</h3>
-                            <p>${robot.category}</p>
-                        </div>
+        <div class="robotdivclass merkez">
+                     <div class="robot">
+                    <img src="${robot.image}" alt="">
+                    <h1>${robot.category}</h1>
+                    <p>${robot.description}</p>
+                    <a class="nam" href="../HTML/dts.html?id=${robot.id}"><button>View details</button></a>
                     </div>
         `
     })
--- JS/robotics.js
+++ JS/robotics.js
@@ -1,18 +1,28 @@
-const robotName = new URLSearchParams(window.location.search).get('id')
-console.log(robotName);
-fetch('http://localhost:3000/robots')
+let page =4;
+const load = document.querySelector(".loadmore")
+const robotssss = document.querySelector('.robotdivclass')
+function showData() {
+    fetch('http://localhost:3000/robots')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        data.forEach(robot => {
-            document.querySelector('.robotdivclass').innerHTML += `
+        data.slice(page - 4,page).forEach(robot => {
+            robotssss.innerHTML += `
                 <div class="robot">
-                    <img src="${robot.img}" alt="">
-                    <h1>${robot.name}</h1>
+                    <img src="${robot.image}" alt="">
+                    <h1>${robot.category}</h1>
                     <p>${robot.description}</p>
-                    <a href="../HTML/dts.html?id=${robot.id}"><button>View details</button></a
-                    >
+                    <a class="nam" href="../HTML/dts.html?id=${robot.id}"><button sat>View details</button></a>
+                    <button "onclick="deleteRobot(${robot.id})">Delete</button>
+                    <button "onclick="updateRobot(${robot.id})">update</button>
                 </div>
             `;
         });
     });
+}
+showData()
+load.addEventListener("click",()=>{
+    console.log("dsjdsoi");
+    page+=4
+    console.log(page);
+    showData()
+})
